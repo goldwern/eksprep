3ee858c657d3667630ca42c6c2dd35ff08f98867
goldwern <githubwg@wgold.de>
2021-04-08 20:23:17 +0200

    first commit

---
+++ prepmicro.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+################################################
+# Prepare "nolike"
+################################################
+
+cd ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app
+cp -r monolith-service nolike-service
+sed -i -e 's/^# @app.route/@app.route/' \
+	-e 's/^# def fulfillLikeMysfit/def fulfillLikeMysfit/' \
+	-e 's/^#     serviceResponse/    serviceResponse/' \
+	-e 's/^#     flaskResponse/    flaskResponse/' \
+	-e 's/^#     return flaskResponse/    return flaskResponse/' \
+	nolike-service/service/mythicalMysfitsService.py
+cd nolike-service/
+docker build -t monolith-service:nolike .
+docker tag monolith-service:nolike ${ECR_Mono}:nolike
+docker push ${ECR_Mono}:nolike
+#################################
+# Prepare "like" service"
+#################################
+export ECR_Like=`aws ecr describe-repositories | grep "repositoryUri.*like" | \
+tr -s " " | cut -f3 -d" " | sed 's/[",]//g'`
+cd ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/like-service
+docker build -t like-service .
+docker tag like-service:latest ${ECR_Like}:latest
+docker push ${ECR_Like}:latest
+
+
